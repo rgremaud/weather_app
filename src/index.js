489093304656260e66c9befa87fdb2aa97b8e707
Romain Gremaud <rgremau@gmail.com>
2025-12-29 12:44:36 -0500

    Rework button click

--- src/index.js
+++ src/index.js
@@ -4,14 +4,35 @@ function formClick() {
     const button = document.getElementById("submit")
 
     button.addEventListener("click", () => {
-        // const content = document.getElementById("content");
-        // content.innerHTML = "";
-
         event.preventDefault();
+
+        // assign constant to location details, current weather and future weather
+        const locationDetails = document.getElementById("locationDetails");
+        const currentWeather = document.getElementById("currentDay");
+        const futureWeather = document.getElementById("futureDays");
+
+        // clear any existing content
+        locationDetails.textContent = "";
+        currentWeather.textContent = "";
+        futureWeather.textContent = "";
+
+        // build new titles
+        buildTitle(locationDetails, "Location Details:");
+        buildTitle(currentWeather, "Current Weather:");
+        buildTitle(futureWeather, "Future Weather:");
+
+        // pull data
         const input = document.getElementById("location");
         printData(input.value);
         input.value = "";
     })
+}
+
+function buildTitle(parentDiv, titleText) {
+    const title = document.createElement("h3");
+    title.textContent = titleText
+
+    parentDiv.appendChild(title);
 }
 
 formClick();
